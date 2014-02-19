14892715bb8cf56a9537bc9986e968d9b7f63c67
Thumb Dick <thumbdick@gmail.com>
2014-02-19 23:31:36 +0000

    update 2

--- web.js
+++ web.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 var fs = require('fs');
-var buf = new Buffer(19);
-buf =  fs.readfile('index.html');
+//var buf = new Buffer(19);
+//buf =  fs.readfile('index.html');
 
 
 app.get('/', function(request, response) {
